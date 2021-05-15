b45eb39de765661a310907df53a518e60a9d2ef1
Munesh <munesh.lalwani1@gmail.com>
2021-05-15 13:36:33 +0500

    Importing Files

---
+++ CrudOpperations/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CrudOpperations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudentRecorddataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.txtSName = new System.Windows.Forms.TextBox();
+            this.Deletebutton = new System.Windows.Forms.Button();
+            this.Resetbutton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRollNo = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentRecorddataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentRecorddataGridView
+            // 
+            this.StudentRecorddataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentRecorddataGridView.Location = new System.Drawing.Point(12, 247);
+            this.StudentRecorddataGridView.Name = "StudentRecorddataGridView";
+            this.StudentRecorddataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.StudentRecorddataGridView.Size = new System.Drawing.Size(559, 150);
+            this.StudentRecorddataGridView.TabIndex = 0;
+            this.StudentRecorddataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentRecorddataGridView_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "S.Name";
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(24, 202);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(75, 39);
+            this.InsertButton.TabIndex = 2;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // txtSName
+            // 
+            this.txtSName.Location = new System.Drawing.Point(63, 26);
+            this.txtSName.Name = "txtSName";
+            this.txtSName.Size = new System.Drawing.Size(129, 20);
+            this.txtSName.TabIndex = 3;
+            this.txtSName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Deletebutton
+            // 
+            this.Deletebutton.Location = new System.Drawing.Point(326, 202);
+            this.Deletebutton.Name = "Deletebutton";
+            this.Deletebutton.Size = new System.Drawing.Size(75, 39);
+            this.Deletebutton.TabIndex = 2;
+            this.Deletebutton.Text = "Delete";
+            this.Deletebutton.UseVisualStyleBackColor = true;
+            this.Deletebutton.Click += new System.EventHandler(this.Deletebutton_Click);
+            // 
+            // Resetbutton
+            // 
+            this.Resetbutton.Location = new System.Drawing.Point(477, 202);
+            this.Resetbutton.Name = "Resetbutton";
+            this.Resetbutton.Size = new System.Drawing.Size(75, 39);
+            this.Resetbutton.TabIndex = 2;
+            this.Resetbutton.Text = "Reset";
+            this.Resetbutton.UseVisualStyleBackColor = true;
+            this.Resetbutton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(175, 202);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(75, 39);
+            this.UpdateButton.TabIndex = 2;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RollNo";
+            // 
+            // txtRollNo
+            // 
+            this.txtRollNo.Location = new System.Drawing.Point(63, 84);
+            this.txtRollNo.Name = "txtRollNo";
+            this.txtRollNo.Size = new System.Drawing.Size(129, 20);
+            this.txtRollNo.TabIndex = 3;
+            this.txtRollNo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(63, 146);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(129, 20);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(380, 26);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(121, 20);
+            this.txtFName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(330, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "F.Name";
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(380, 119);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(121, 20);
+            this.txtMobile.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(330, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mobile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(583, 409);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtRollNo);
+            this.Controls.Add(this.txtSName);
+            this.Controls.Add(this.Resetbutton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.Deletebutton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StudentRecorddataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crud App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentRecorddataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentRecorddataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.TextBox txtSName;
+        private System.Windows.Forms.Button Deletebutton;
+        private System.Windows.Forms.Button Resetbutton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRollNo;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CrudOpperations/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudOpperations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-GFMM88C\SQLEXPRESS;Initial Catalog=Crudsharp;Integrated Security=True");
+        public int StudentId;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Reset();
+
+        }
+
+        private void Reset()
+        {
+            StudentId = 0;
+            txtSName.Clear();
+            txtFName.Clear();
+            txtRollNo.Clear();
+            txtAddress.Clear();
+            txtMobile.Clear();
+            txtSName.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetStudentRecord();
+
+        }
+
+        private void GetStudentRecord()
+        {
+           
+            SqlCommand cmd = new SqlCommand("select * from StudentTb", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            StudentRecorddataGridView.DataSource = dt;
+
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            if (isValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO StudentTb VALUES (@Name,@FatherName,@RollNumber,@Address,@Mobile)",con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Name", txtSName.Text);
+                cmd.Parameters.AddWithValue("@FatherName", txtFName.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", txtRollNo.Text);
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("New Record save" ,"Inserted Record",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                GetStudentRecord();
+                Reset();
+            }
+        }
+
+        private bool isValid()
+        {
+            if(txtSName.Text == "")
+            {
+                MessageBox.Show("Student name is required", "Failed", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void StudentRecorddataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            StudentId = Convert .ToInt32(StudentRecorddataGridView.SelectedRows[0].Cells[0].Value);
+            txtSName.Text = StudentRecorddataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            txtFName.Text = StudentRecorddataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            txtRollNo.Text = StudentRecorddataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            txtAddress.Text = StudentRecorddataGridView.SelectedRows[0].Cells[4].Value.ToString();
+            txtMobile.Text = StudentRecorddataGridView.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (StudentId > 0)
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE StudentTb SET Name = @Name, FatherName = @FatherName,@RollNumber = RollNumber,Address  = @Address,Mobile = @Mobile WHERE StudentID = @ID", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Name", txtSName.Text);
+                cmd.Parameters.AddWithValue("@FatherName", txtFName.Text);
+                cmd.Parameters.AddWithValue("@RollNumber", txtRollNo.Text);
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+                cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@ID", this.StudentId);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Information Updated", "Updated Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentRecord();
+                Reset();
+            }
+            else
+            {
+                MessageBox.Show("Plz select", "select ? Record", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Deletebutton_Click(object sender, EventArgs e)
+        {
+            if (StudentId > 0)
+            {
+                SqlCommand cmd = new SqlCommand("DELETE StudentTb  WHERE StudentID = @ID", con);
+                cmd.CommandType = CommandType.Text;
+              
+                cmd.Parameters.AddWithValue("@ID", this.StudentId);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Deleted", "Deleted Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                GetStudentRecord();
+                Reset();
+            }
+
+            else
+            {
+                MessageBox.Show("Plz select for delete", "select ? Record", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
